df23b2e42e7527922178eb375d387530b8028ecb
Roman <topor13@mail.ru>
2018-02-06 11:29:49 +0300

    add empty activities for all tasks

--- app/src/main/java/ru/sberbank/awsapp/MainActivity.java
+++ app/src/main/java/ru/sberbank/awsapp/MainActivity.java
@@ -8,17 +8,37 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     Button pagePreviewBtn;
+    Button rssBtn;
+    Button weatherBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         pagePreviewBtn = (Button) findViewById(R.id.previewButton);
+        rssBtn = (Button) findViewById(R.id.rssButton);
+        weatherBtn = (Button) findViewById(R.id.weatherButton);
 
         pagePreviewBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, PagePreviewActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        rssBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RssActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        weatherBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, WeatherActivity.class);
                 startActivity(intent);
             }
         });
